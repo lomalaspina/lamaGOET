a0d86db5785c774aaa74f95ad19610035b344582
Lorraine A. Malaspina <lorraine.malaspina@gmail.com>
2019-07-29 15:05:21 +0200

    full installatio folder with current version

---
+++ instalation_july18/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo cp Tonto_logo.png /usr/local/include/
+sudo apt-get install gtk+-2.0 -y
+sudo apt-get install gtkdialog -y
+sudo apt-get install gawk -y
+sudo apt-get install zenity -y
+cd gtkdialog-0.8.3
+./configure
+make
+sudo make install
+cd ..
+LOCALDIR=$(pwd)
+cd /usr/local/bin
+sudo ln -s $LOCALDIR/lamaGOET lamaGOET
+cd $LOCALDIR
