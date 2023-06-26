d82afdd93be3fc99dc21643b056cfe11b74a62f1
lomalaspina <lorraine.malaspina@gmail.com>
2023-06-26 13:24:40 +0200

    modified instalation script to link the original sh

--- instalation_July19/install.sh
+++ instalation_July19/install.sh
@@ -7,15 +7,18 @@ sudo apt-get install gtkdialog -y
 sudo apt-get install gawk -y
 sudo apt-get install zenity -y
 cd gtkdialog-0.8.3
+cd src
+cp gtkdialog gtkdialog_pre_compiled
+cd ..
 ./configure
 make
 sudo make install
 cd ..
 LOCALDIR=$(pwd)
 cd /usr/local/bin
-sudo ln -s $LOCALDIR/lamaGOET lamaGOET
-#sudo ln -s $LOCALDIR/hklfromm80.py hklfromm80.py
-#sudo ln -s $LOCALDIR/projectioninputfromhar.py projectioninputfromhar.py 
-sudo ln -s $LOCALDIR/powderHARstart.py powderHARstart.py
-sudo ln -s $LOCALDIR/powderHARcifrewrite.py powderHARcifrewrite.py
+sudo ln -s $LOCALDIR/lamaGOET.sh lamaGOET
+##sudo ln -s $LOCALDIR/hklfromm80.py hklfromm80.py
+##sudo ln -s $LOCALDIR/projectioninputfromhar.py projectioninputfromhar.py 
+#sudo ln -s $LOCALDIR/powderHARstart.py powderHARstart.py
+#sudo ln -s $LOCALDIR/powderHARcifrewrite.py powderHARcifrewrite.py
 cd $LOCALDIR
